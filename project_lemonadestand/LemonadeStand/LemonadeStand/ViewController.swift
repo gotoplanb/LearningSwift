890379ef4e49e9b06da7c3fbc26794a4e331a7b1
Dave Stanton <davestanton.us@gmail.com>
2015-02-16 12:48:47 -0500

    connections solution

--- project_lemonadestand/LemonadeStand/LemonadeStand/ViewController.swift
+++ project_lemonadestand/LemonadeStand/LemonadeStand/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var moneySupplyCount: UILabel!
+    @IBOutlet weak var lemonSupplycount: UILabel!
+    @IBOutlet weak var iceCubeSupplyCount: UILabel!
+    @IBOutlet weak var lemonPurchaseCount: UILabel!
+    @IBOutlet weak var iceCubePurchaseCount: UILabel!
+    @IBOutlet weak var lemonMixCount: UILabel!
+    @IBOutlet weak var iceCubeMixCount: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func purchaseLemonButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func unpurchaseLemonButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func purchaseIceCubeButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func unpurchaseIceCubeButtonPressed(sender: UIButton) {
+    }
+    
+    
+
+    
+    @IBAction func mixLemonButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func mixIceCubeButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func unmixLemonButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func unmixIceCubeButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func startDayButtonPressed(sender: UIButton) {
+    }
 
 }
 
