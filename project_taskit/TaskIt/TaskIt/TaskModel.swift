2e4d6456045944d0c3694597f8c0df676e1d29d0
Dave Stanton <davestanton.us@gmail.com>
2015-02-16 21:25:41 -0500

    implementing the TaskModel

---
+++ project_taskit/TaskIt/TaskIt/TaskModel.swift
@@ -0,0 +1,15 @@
+//
+//  TaskModel.swift
+//  TaskIt
+//
+//  Created by David Stanton on 2/16/15.
+//  Copyright (c) 2015 David Stanton. All rights reserved.
+//
+
+import Foundation
+
+struct TaskModel {
+    var task: String
+    var subTask: String
+    var date: String
+}
--- project_taskit/TaskIt/TaskIt/ViewController.swift
+++ project_taskit/TaskIt/TaskIt/ViewController.swift
@@ -12,28 +12,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
-    var taskArray: [Dictionary<String,String>] = []
+    var taskArray: [TaskModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let task1: Dictionary<String, String> = [
-            "task": "Study French",
-            "subtask": "Verbs",
-            "date": "01/14/2014"
+        
+        let task1 = TaskModel(task: "Study French", subTask: "Verbs", date: "01/14/2014")
+        let task2 = TaskModel(task: "Eat Dinner", subTask: "burgers", date: "01/14/2014")
+        
+        taskArray = [
+            task1,
+            task2,
+            TaskModel(task: "Gym", subTask: "leg day", date: "01/14/2014")
         ]
-        let task2: Dictionary<String, String> = [
-            "task": "Eat Dinner",
-            "subtask": "burgers",
-            "date": "01/14/2014"
-        ]
-        let task3: Dictionary<String, String> = [
-            "task": "Gym",
-            "subtask": "leg day",
-            "date": "01/14/2014"
-        ]
-        taskArray = [task1, task2, task3]
-        println(task1["task"])
+
         self.tableView.reloadData()
     }
 
@@ -49,16 +42,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-
-        println(indexPath.row)
         
-        let taskDict: Dictionary = taskArray[indexPath.row]
+        let thisTask = taskArray[indexPath.row]
         
         var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell
         
-        cell.taskLabel.text = taskDict["task"]
-        cell.descriptionLabel.text =  taskDict["subtask"]
-        cell.dateLabel.text = taskDict["date"]
+        cell.taskLabel.text = thisTask.task
+        cell.descriptionLabel.text = thisTask.subTask
+        cell.dateLabel.text = thisTask.date
         
         return cell
 
