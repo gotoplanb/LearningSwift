53c2545acedb73f6589682edc9896dcd81acb35f
Dave Stanton <davestanton.us@gmail.com>
2015-02-16 21:34:18 -0500

    TaskDetailViewController

---
+++ project_taskit/TaskIt/TaskIt/TaskDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TaskDetailViewController.swift
+//  TaskIt
+//
+//  Created by David Stanton on 2/16/15.
+//  Copyright (c) 2015 David Stanton. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
