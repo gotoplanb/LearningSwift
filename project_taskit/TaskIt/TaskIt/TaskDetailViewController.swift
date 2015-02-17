708d1965fb9c978bf7bea80874bb4a98b65da5c9
Dave Stanton <davestanton.us@gmail.com>
2015-02-16 22:09:10 -0500

    Updating the DetailTaskViewController

--- project_taskit/TaskIt/TaskIt/TaskDetailViewController.swift
+++ project_taskit/TaskIt/TaskIt/TaskDetailViewController.swift
@@ -12,12 +12,17 @@ class TaskDetailViewController: UIViewController {
 
     var detailTaskModel: TaskModel!
     
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var subtaskTextField: UITextField!
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        println(detailTaskModel.task)
+        self.taskTextField.text = detailTaskModel.task
+        self.subtaskTextField.text = detailTaskModel.task
         
     }
 
