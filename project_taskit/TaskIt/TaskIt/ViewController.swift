efb0334e84b4585522c19dc05e17537f19fddf7e
Dave Stanton <davestanton.us@gmail.com>
2015-02-23 23:53:30 -0500

    Completing tasks

--- project_taskit/TaskIt/TaskIt/ViewController.swift
+++ project_taskit/TaskIt/TaskIt/ViewController.swift
@@ -117,6 +117,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+
+        let thisTask = baseArray[indexPath.section][indexPath.row]
+
+        var doneTask = TaskModel(task: thisTask.task, subTask: thisTask.subTask, date: thisTask.date, completed: true)
+        
+        baseArray[indexPath.section].removeAtIndex(indexPath.row)
+        baseArray[1].append(doneTask)
+        tableView.reloadData()
+    
+    }
+    
     // Helpers
     
     
