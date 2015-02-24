7345b344566cb4e7dedde40d302c286fa3d9d4dc
Dave Stanton <davestanton.us@gmail.com>
2015-02-23 23:35:46 -0500

    Finish fixing the errors

--- project_taskit/TaskIt/TaskIt/ViewController.swift
+++ project_taskit/TaskIt/TaskIt/ViewController.swift
@@ -36,7 +36,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
 
-        taskArray = taskArray.sorted {
+        baseArray[0] = baseArray[0].sorted {
             (taskOne:TaskModel, taskTwo:TaskModel) -> Bool in
             return taskOne.date.timeIntervalSince1970 < taskTwo.date.timeIntervalSince1970
             
