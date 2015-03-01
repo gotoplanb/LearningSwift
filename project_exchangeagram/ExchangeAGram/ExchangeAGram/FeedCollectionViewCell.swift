dc4d62911f9cec47fb09d0f48831a60ad8aead1d
Dave Stanton <davestanton.us@gmail.com>
2015-02-28 22:32:28 -0500

    FeedCell

---
+++ project_exchangeagram/ExchangeAGram/ExchangeAGram/FeedCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FeedCollectionViewCell.swift
+//  ExchangeAGram
+//
+//  Created by Dave Stanton on 2/28/15.
+//  Copyright (c) 2015 Dave Stanton. All rights reserved.
+//
+
+import UIKit
+
+class FeedCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
