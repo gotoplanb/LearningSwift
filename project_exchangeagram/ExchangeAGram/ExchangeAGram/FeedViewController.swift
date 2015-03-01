aa9a6a3a08aa3f72369fc2d004154d4460747e46
Dave Stanton <davestanton.us@gmail.com>
2015-02-28 23:02:12 -0500

    PhotoLibrary

--- project_exchangeagram/ExchangeAGram/ExchangeAGram/FeedViewController.swift
+++ project_exchangeagram/ExchangeAGram/ExchangeAGram/FeedViewController.swift
@@ -46,6 +46,20 @@ class FeedViewController: UIViewController, UICollectionViewDataSource, UICollec
             cameraController.allowsEditing = false
             self.presentViewController(cameraController, animated: true, completion: nil)
         }
+        else if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            var photoLibraryController = UIImagePickerController()
+            photoLibraryController.delegate = self
+            photoLibraryController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            let mediaTypes: [AnyObject] = [kUTTypeImage]
+            photoLibraryController.mediaTypes = mediaTypes
+            photoLibraryController.allowsEditing = false
+            self.presentViewController(photoLibraryController, animated: true, completion: nil)
+        }
+        else {
+            var alertController = UIAlertController(title: "Alert", message: "Your device does not support the camera or photo library.", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
         
     }
     
