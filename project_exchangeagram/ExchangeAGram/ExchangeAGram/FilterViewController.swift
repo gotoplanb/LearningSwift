5571a939815978330cac48ea85dc1719941508fa
Dave Stanton <davestanton.us@gmail.com>
2015-04-04 18:02:35 -0400

    Introduction to GCD

--- project_exchangeagram/ExchangeAGram/ExchangeAGram/FilterViewController.swift
+++ project_exchangeagram/ExchangeAGram/ExchangeAGram/FilterViewController.swift
@@ -51,8 +51,18 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         
         let cell: FilterCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("FilterCell", forIndexPath: indexPath) as FilterCollectionViewCell
         
-//        cell.imageView.image = UIImage(named: "Placeholder")
-        cell.imageView.image = filteredImageFromImage(thisFeedItem.image, filter: filters[indexPath.row])
+        cell.imageView.image = UIImage(named: "Placeholder")
+        
+        let filterQueue:dispatch_queue_t = dispatch_queue_create("filter queue", nil)
+        
+        dispatch_async(filterQueue, { () -> Void in
+            let filterImage = self.filteredImageFromImage(self.thisFeedItem.image, filter: self.filters[indexPath.row])
+        
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                cell.imageView.image = filterImage
+            })
+        
+        })
         
         return cell
         
